8adc5544bac88db05ba351e2ddaa4b43b8e1916c
brait0n <pellencbrian@gmail.com>
2022-06-19 16:26:38 -0300

    Se agrega funcionalidad a la barra de navegación (NAVBAR)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,7 @@
   <nav class="navbar bg-light">
     <div class="container-fluid">
       <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-outline-success" type="submit">NAVBAR: Recargar pagina para testear funcionalidades</button>
       </form>
     </div>
   </nav>
@@ -16,5 +15,15 @@ export default {
       type: String
     }
   },
+  data() {
+    return {
+      search: ''
+    }
+  },
+  methods: {
+    searchMovie() {
+      this.$emit('search', this.search)
+    }
+  }
 }
 </script>
